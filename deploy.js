e5e8dcd0136d4b98dd96effd371fb11006a332e9
Punit Jajodia <punit@parewalabs.com>
2016-07-04 22:35:24 +0545

    Modify deploy script to show stdout as well

--- deploy.js
+++ deploy.js
@@ -2,8 +2,9 @@ var path = require('path');
 var execFile = require('child_process').execFile;
 
 var exePath = path.resolve(__dirname, './deploy.sh');
-execFile(exePath, function(error, stderr) {
-   console.log('stderr: ', __dirname);
+execFile(exePath, function(error, stdout, stderr) {
+  console.log('stdout: ' + stdout);
+  console.log('stderr: ', stderr);
    if (error !== null) {
        console.log('exec error: ', error);
    }
